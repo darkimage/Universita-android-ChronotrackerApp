c7804a494282a996391d693a56858768cb93f12f
DarkImage <luc-af@live.it>
2019-08-17 22:03:00 +0200

    First Commit

---
+++ app/src/main/java/unipr/luc_af/chronotracker/MainActivity.java
@@ -0,0 +1,14 @@
+package unipr.luc_af.chronotracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
